c0d926ba6c7d1bdbc6ddbffcdf6453f3de561ce0
yulahabogoslavets <speed0435@gmail.com>
2020-04-17 16:24:01 +0200

    Updates

---
+++ precache-manifest.d338284c1906fff471864e24d6e8a72f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e28b6f938d340f03ae171d5ebda0cdb",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "55b0bdce1b28076c263f",
+    "url": "/react-todolist/static/css/main.f1074330.chunk.css"
+  },
+  {
+    "revision": "264f7792042a2bb650d6",
+    "url": "/react-todolist/static/js/2.ec7b4d4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolist/static/js/2.ec7b4d4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55b0bdce1b28076c263f",
+    "url": "/react-todolist/static/js/main.e9fcd3c6.chunk.js"
+  },
+  {
+    "revision": "9cfe7a6e69363667a69d",
+    "url": "/react-todolist/static/js/runtime-main.83a6c84e.js"
+  }
+]);
